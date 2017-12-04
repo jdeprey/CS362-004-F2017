033534dc5f6fa7f64e4e6b8bd2c3ab10bfbe064a
Joseph DePrey <joe.deprey@gmail.com>
2017-12-03 18:02:23 -0800

    Add updated UrlValidatorTest

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -17,8 +17,9 @@
 
 
 import junit.framework.TestCase;
-import java.util.ArrayList;
-import java.util.List;
+
+
+
 
 
 /**
@@ -28,189 +29,52 @@ import java.util.List;
  */
 public class UrlValidatorTest extends TestCase {
 
-    private boolean printStatus = false;
-    private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
-    private List<String> validList = new ArrayList<>();
-    private List<String> invalidList = new ArrayList<>();
+   private boolean printStatus = false;
+   private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
 
-    public UrlValidatorTest(String testName) {
-        super(testName);
-    }
+   public UrlValidatorTest(String testName) {
+      super(testName);
+   }
 
 
-    public void testManualTest() {
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-        System.out.println(urlVal.isValid("http://www.amazon.com"));
-    }
 
-    private void testURL(String url, UrlValidator urlValidator, List<String> valid, List<String> invalid) {
-        if (urlValidator.isValid(url)) {
-            valid.add(url);
-        } else {
-            invalid.add(url);
-        }
-    }
-
-    public void testSchemePartition() {
-        ResultPair[] testSchemes = {
-                new ResultPair("http://", true),
-                new ResultPair("ftp://", true),
-                new ResultPair("h3t://", true),
-                new ResultPair("", true),
-                new ResultPair("3ht://", false),
-                new ResultPair("http:/", false),
-                new ResultPair("http:", false),
-                new ResultPair("http/", false),
-                new ResultPair("://", false)
-        };
-
-        final String URL = "www.google.com";
-        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-
-        for (int i = 0; i < testSchemes.length; i++) {
-            testURL(testSchemes[i].item + URL, urlValidator, validList, invalidList);
-        }
-
-        System.out.println("============== testSchemePartition ==============\n");
-        System.out.println("Expected valid scheme count: " + 4 + ", actual valid scheme count: " + validList.size());
-        System.out.println("Expected invalid scheme count: " + 5 + ", actual invalid scheme count: " + invalidList.size());
-
-        System.out.print("Valid schemes: ");
-        for (String scheme : validList) {
-            System.out.print(scheme + ", ");
-        }
-
-        System.out.println();
-
-        System.out.print("Invalid schemes: ");
-        for (String scheme : invalidList) {
-            System.out.print(scheme + ", ");
-        }
-
-        // clean up lists
-        validList.clear();
-        invalidList.clear();
-    }
-
-    public void testAuthorityPartition() {
-        ResultPair[] testAuthority = {
-                new ResultPair("www.google.com", true),
-                new ResultPair("go.com", true),
-                new ResultPair("go.au", true),
-                new ResultPair("0.0.0.0", true),
-                new ResultPair("go.cc", true),
-                new ResultPair("255.255.255.255", true),
-                new ResultPair("255.com", true),
-                new ResultPair("256.256.256.256", false),
-                new ResultPair("1.2.3.4.5", false),
-                new ResultPair("1.2.3.4.", false),
-                new ResultPair("1.2.3", false),
-                new ResultPair(".1.2.3.4", false),
-                new ResultPair("go.a", false),
-                new ResultPair("go.a1a", false),
-                new ResultPair("go.1aa", false),
-                new ResultPair("aaa.", false),
-                new ResultPair(".aaa", false),
-                new ResultPair("aaa", false),
-                new ResultPair("", false)
-        };
-
-        final String SCHEME = "http://";
-        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
-
-        for (int i = 0; i < testAuthority.length; i++) {
-            testURL(SCHEME + testAuthority[i].item, urlValidator, validList, invalidList);
-        }
-
-        System.out.println("============== testSchemePartition ==============\n");
-        System.out.println("Expected valid authority count: " + 7 + ", actual valid authority count: " + validList.size());
-        System.out.println("Expected invalid authority count: " + 12 + ", actual invalid authority count: " + invalidList.size());
-
-        System.out.print("Valid authorities: ");
-        for (String auth : validList) {
-            System.out.print(auth + ", ");
-        }
-
-        System.out.println();
-
-        System.out.print("Invalid authorities: ");
-        for (String auth : invalidList) {
-            System.out.print(auth + ", ");
-        }
-
-        // clean up lists
-        validList.clear();
-        invalidList.clear();
-    }
-
-    public void testPortPartition() {
-
-    }
-
-    public void testPathPartition() {
-
-    }
-
-    public void testQueryPartition() {
-
-    }
-
-    public void testIsValid() {
-        for (int i = 0; i < 10000; i++) {
-
-        }
-    }
-
-    public void testAnyOtherUnitTest() {
-
-    }
-
-    public void testManualValidator() {
-        ResultPair[] testURLS = {
-                new ResultPair("https://www.google.com", true),
-                new ResultPair("https://google.com", true),
-                new ResultPair("http://localhost/", true),
-                new ResultPair("https://///www.google.com", false),
-                new ResultPair("htps://www.google.com", false),
-                new ResultPair("https:google.com", false),
-                new ResultPair("http://google.com___.com", false)
-        };
-
-        String[] schemes = {"http", "https"};
-        UrlValidator urlValidator = new UrlValidator(schemes, UrlValidator.ALLOW_LOCAL_URLS);
-
-        for (int i = 0; i < testURLS.length; i++) {
-            testURL(testURLS[i].item, urlValidator, validList, invalidList);
-        }
-
-        System.out.println("============== testManualValidator ==============\n");
-        System.out.println("Expected valid url count: " + 3 + ", actual valid url count: " + validList.size());
-        System.out.println("Expected invalid url count: " + 4 + ", actual invalid url count: " + invalidList.size());
-
-        System.out.print("Valid URLS: ");
-        for (String url : validList) {
-            System.out.print(url + ", ");
-        }
-
-        System.out.println();
-
-        System.out.print("Invalid URLS: ");
-        for (String url : invalidList) {
-            System.out.print(url + ", ");
-        }
-
-        // clean up lists
-        validList.clear();
-        invalidList.clear();
-    }
+   public void testManualTest()
+   {
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   System.out.println(urlVal.isValid("http://www.amazon.com"));
 
 
-    /**
-     * Create set of tests by taking the testUrlXXX arrays and
-     * running through all possible permutations of their combinations.
-     *
-     * @param testObjects Used to create a url.
-     */
+   }
+
+
+   public void testYourFirstPartition()
+   {
+
+   }
+
+   public void testYourSecondPartition(){
+
+   }
+
+
+   public void testIsValid()
+   {
+	   for(int i = 0;i<10000;i++)
+	   {
+
+	   }
+   }
+
+   public void testAnyOtherUnitTest()
+   {
+
+   }
+   /**
+    * Create set of tests by taking the testUrlXXX arrays and
+    * running through all possible permutations of their combinations.
+    *
+    * @param testObjects Used to create a url.
+    */
 
 
 }
--- projects/depreyj/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ projects/depreyj/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -17,9 +17,9 @@
 
 
 import junit.framework.TestCase;
-
-
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 /**
@@ -29,52 +29,259 @@ import junit.framework.TestCase;
  */
 public class UrlValidatorTest extends TestCase {
 
-   private boolean printStatus = false;
-   private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
+    private boolean printStatus = false;
+    private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
+    private List<String> validList = new ArrayList<>();
+    private List<String> invalidList = new ArrayList<>();
 
-   public UrlValidatorTest(String testName) {
-      super(testName);
-   }
+    public UrlValidatorTest(String testName) {
+        super(testName);
+    }
 
-   
-   
-   public void testManualTest()
-   {
-	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
-	   
-	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
-	   
-   }
-   
-   public void testYourSecondPartition(){
-	   
-   }
-   
-   
-   public void testIsValid()
-   {
-	   for(int i = 0;i<10000;i++)
-	   {
-		   
-	   }
-   }
-   
-   public void testAnyOtherUnitTest()
-   {
-	   
-   }
-   /**
-    * Create set of tests by taking the testUrlXXX arrays and
-    * running through all possible permutations of their combinations.
-    *
-    * @param testObjects Used to create a url.
-    */
-   
 
+    public void testManualTest() {
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        System.out.println(urlVal.isValid("http://www.amazon.com"));
+    }
+
+    private void testURL(String url, UrlValidator urlValidator, List<String> valid, List<String> invalid) {
+        if (urlValidator.isValid(url)) {
+            valid.add(url);
+        } else {
+            invalid.add(url);
+        }
+    }
+
+    private void printResults(int validCount, int invalidCount) {
+        System.out.println("Expected valid count: " + validCount + ", actual valid count: " + validList.size());
+        System.out.println("Expected invalid count: " + invalidCount + ", actual invalid count: " + invalidList.size());
+
+        System.out.print("Valid: ");
+        for (String auth : validList) {
+            System.out.print(auth + ", ");
+        }
+
+        System.out.println();
+
+        System.out.print("Invalid: ");
+        for (String auth : invalidList) {
+            System.out.print(auth + ", ");
+        }
+
+        // clean up lists
+        validList.clear();
+        invalidList.clear();
+    }
+
+    private void printResults() {
+        System.out.print("Valid: ");
+        for (String auth : validList) {
+            System.out.print(auth + ", ");
+        }
+
+        System.out.println();
+
+        System.out.print("Invalid: ");
+        for (String auth : invalidList) {
+            System.out.print(auth + ", ");
+        }
+
+        // clean up lists
+        validList.clear();
+        invalidList.clear();
+    }
+
+    public void testSchemePartition() {
+        ResultPair[] testSchemes = {
+                new ResultPair("http://", true),
+                new ResultPair("ftp://", true),
+                new ResultPair("h3t://", true),
+                new ResultPair("", true),
+                new ResultPair("3ht://", false),
+                new ResultPair("http:/", false),
+                new ResultPair("http:", false),
+                new ResultPair("http/", false),
+                new ResultPair("://", false)
+        };
+
+        final String URL = "www.google.com";
+        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+
+        for (int i = 0; i < testSchemes.length; i++) {
+            testURL(testSchemes[i].item + URL, urlValidator, validList, invalidList);
+        }
+
+        System.out.println("\n\n============== testSchemePartition ==============\n");
+        printResults(4, 5);
+    }
+
+    public void testAuthorityPartition() {
+        ResultPair[] testAuthority = {
+                new ResultPair("www.google.com", true),
+                new ResultPair("go.com", true),
+                new ResultPair("go.au", true),
+                new ResultPair("0.0.0.0", true),
+                new ResultPair("go.cc", true),
+                new ResultPair("255.255.255.255", true),
+                new ResultPair("255.com", true),
+                new ResultPair("256.256.256.256", false),
+                new ResultPair("1.2.3.4.5", false),
+                new ResultPair("1.2.3.4.", false),
+                new ResultPair("1.2.3", false),
+                new ResultPair(".1.2.3.4", false),
+                new ResultPair("go.a", false),
+                new ResultPair("go.a1a", false),
+                new ResultPair("go.1aa", false),
+                new ResultPair("aaa.", false),
+                new ResultPair(".aaa", false),
+                new ResultPair("aaa", false),
+                new ResultPair("", false)
+        };
+
+        final String SCHEME = "http://";
+        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+
+        for (int i = 0; i < testAuthority.length; i++) {
+            testURL(SCHEME + testAuthority[i].item, urlValidator, validList, invalidList);
+        }
+
+        System.out.println("\n\n============== testAuthorityPartition ==============\n");
+        printResults(7, 12);
+    }
+
+    public void testPortPartition()
+    {
+        ResultPair[] testPort = {
+                new ResultPair(":80", true),
+                new ResultPair(":65535", true),
+                new ResultPair(":0", true),
+                new ResultPair("", true),
+                new ResultPair(":-1", false),
+                new ResultPair(":65636", true),
+                new ResultPair(":65a", false)
+        };
+
+        final String SCHEME = "http://";
+        final String URL = "www.google.com";
+        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+
+        for (int i = 0; i < testPort.length; i++) {
+            testURL(SCHEME + URL + testPort[i].item, urlValidator, validList, invalidList);
+        }
+
+        System.out.println("\n\n============== testPortPartition ==============\n");
+        printResults(5, 2);
+    }
+
+    /*
+     *  Research:
+     *      https://stackoverflow.com/questions/1547899/which-characters-make-a-url-invalid
+     */
+    public void testPathPartition() {
+        ResultPair[] testPath = {
+                new ResultPair("", true),
+                new ResultPair("/abc", true),
+                new ResultPair("/abc1", true),
+                new ResultPair("/abc123", true),
+                new ResultPair("/abc-123", true),
+                new ResultPair("/abc_123", true),
+                new ResultPair("/abc/123", true),
+                new ResultPair("/abc/123/def", true),
+                new ResultPair("/abc//123", false),
+                new ResultPair("/abc//123", false),
+                new ResultPair("/abc^123", false),
+                new ResultPair("/abc{123", false),
+                new ResultPair("/abc}123", false),
+                new ResultPair("/abc|123", false),
+                new ResultPair("/abc>123", false),
+                new ResultPair("/abc<123", false),
+        };
+        final String SCHEME = "http://";
+        final String URL = "www.google.com";
+        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+
+        for (ResultPair aTestPath : testPath) {
+            testURL(SCHEME + URL + aTestPath.item, urlValidator, validList, invalidList);
+        }
+
+        System.out.println("\n\n============== testPathPartition ==============\n");
+        printResults(8, 8);
+    }
+
+    /*
+     *  Research:
+     *      https://en.wikipedia.org/wiki/Query_string
+     */
+    public void testQueryPartition() {
+        ResultPair[] testQuery = {
+                new ResultPair("", true),
+                new ResultPair("?", true),
+                new ResultPair("search?abc=123", true),
+                new ResultPair("?abc=123", true),
+                new ResultPair("?field1=value1", true),
+                new ResultPair("?field1=value1&field2=value2", true),
+                new ResultPair("page.html?q=1;q=2;q=3", true),
+                new ResultPair("?@bar._=???/1:", true),
+                new ResultPair("file?id=wjVg/\n", true),
+        };
+        final String SCHEME = "http://";
+        final String URL = "www.google.com/";
+        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+
+        for (ResultPair aTestQuery : testQuery) {
+            testURL(SCHEME + URL + aTestQuery.item, urlValidator, validList, invalidList);
+        }
+
+        System.out.println("\n\n============== testQueryPartition ==============\n");
+        printResults(9, 0);
+    }
+
+    public void testIsValid() {
+        String[] schemeInput = {"http://", "h3t://", "http:/", "://"};
+        String[] authorityInput = {"www.google.com", "go.au", ".1.2.3.4", ""};
+        String[] portInput = {":80", ":0", ":-1", ":65a"};
+
+        Random rand = new Random();
+        StringBuilder sb;
+        UrlValidator urlValidator = new UrlValidator(UrlValidator.ALLOW_ALL_SCHEMES);
+
+        for (int i = 0; i < 10000; i++) {
+            int schemeIndex = rand.nextInt(schemeInput.length);
+            int authorityIndex = rand.nextInt(authorityInput.length);
+            int portIndex = rand.nextInt(portInput.length);
+
+            sb = new StringBuilder();
+
+            sb.append(schemeInput[schemeIndex]);
+            sb.append(authorityInput[authorityIndex]);
+            sb.append(portInput[portIndex]);
+
+            testURL(sb.toString(), urlValidator, validList, invalidList);
+        }
+
+        System.out.println("\n\n============== testIsValid ==============\n");
+        printResults();
+    }
+
+    public void testManualValidator() {
+        ResultPair[] testURLS = {
+                new ResultPair("https://www.google.com", true),
+                new ResultPair("https://google.com", true),
+                new ResultPair("http://localhost/", true),
+                new ResultPair("https://///www.google.com", false),
+                new ResultPair("htps://www.google.com", false),
+                new ResultPair("https:google.com", false),
+                new ResultPair("http://google.com___.com", false)
+        };
+
+        String[] schemes = {"http", "https"};
+        UrlValidator urlValidator = new UrlValidator(schemes, UrlValidator.ALLOW_LOCAL_URLS);
+
+        for (int i = 0; i < testURLS.length; i++) {
+            testURL(testURLS[i].item, urlValidator, validList, invalidList);
+        }
+
+        System.out.println("\n\n============== testManualValidator ==============\n");
+        printResults(3, 4);
+    }
 }
